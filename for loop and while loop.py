3a94557be9c3c0a46159c39dcd7b2c9dcce65efb
Ravikant Maurya <120043947+ravikantmaurya00@users.noreply.github.com>
2023-02-19 14:27:26 +0530

    Add files via upload

---
+++ for loop and while loop.py
@@ -0,0 +1,19 @@
+print("for loop with range()")
+for n in range (1,11):
+	print('6*',n,'=',6*n)
+print()
+print('reverse loop')
+for n in range (10,3,-1):
+   print(n)
+print("while loop in python")
+i=10
+while i>=1:
+	print(i,'Ravikant Maurya')
+	i=i-1
+print(i)
+a=20
+while a>=1:
+   
+  print(a,'computer')
+  a=a-1
+print(a)
